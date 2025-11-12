236eed80be72e7c22e9ba1fcbe2e3b0422d8e730
Andrey <rysykolen99@gmail.com>
2025-11-12 22:02:41 +0500

    Commit

--- src/lr1/Example7.java
+++ src/lr1/Example7.java
@@ -1,5 +1,18 @@
 package lr1;
 
-public class Example7 {
+import java.util.Scanner;
 
+public class Example7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input name: ");
+        String name = in.nextLine();
+
+        System.out.println("Input age: ");
+        int age = in.nextInt();
+
+        System.out.println("Данные пользователя");
+        System.out.println("Имя: " + name + " " + "Возраст: " + age);
+    }
 }
