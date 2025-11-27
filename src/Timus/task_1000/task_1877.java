0429184471b91203335f0dc24029e75edbbbb260
Andrey <andrey.maht@yandex.ru>
2025-11-27 18:34:45 +0500

    initial commit

--- src/Timus/task_1000/task_1877.java
+++ src/Timus/task_1000/task_1877.java
@@ -1,7 +1,21 @@
 package Timus.task_1000;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class task_1877 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int codeOne = in.nextInt();
+        int codeTwo = in.nextInt();
+
+        if (codeOne % 2 == 0 || codeTwo % 2 != 0) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
 
     }
 }
