77268c119555f81d576c4c0ac2fc5c5a23e47e70
Andrey <andrey.maht@yandex.ru>
2025-12-04 21:08:28 +0500

    initial commit

--- src/lr3/task_3.java
+++ src/lr3/task_3.java
@@ -1,7 +1,27 @@
 package lr3;
 
+import java.util.Scanner;
+
 public class task_3 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите количество чисел в последовательности: ");
+        int size = in.nextInt();
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 0; i < size; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+
+            System.out.println(next);
+
+        }
+
+
 
     }
 }
