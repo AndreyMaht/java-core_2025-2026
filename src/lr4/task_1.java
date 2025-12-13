149a52574da64bf6d828fa1faaf804616bfc75a5
Andrey <andrey.maht@yandex.ru>
2025-12-13 11:28:28 +0500

    initial commit

--- src/lr4/task_1.java
+++ src/lr4/task_1.java
@@ -2,6 +2,27 @@ package lr4;
 
 public class task_1 {
 
+    public static void main(String[] args) {
+
+        int figure = 15;
+        int i;
+        int j;
+        int z;
+
+        for (i=1; i<= figure; i++) {
+            System.out.println("number string: " + i + " ");
+            z = 0;
+            for (j = -10; j < figure; j++) {
+                System.out.println("+");
+                z = z + 1;
+            }
+            System.out.println("Количество символов в строке" + z);
+        }
+
+    }
+
+
+
 
 
 }
