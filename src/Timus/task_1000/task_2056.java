a56c4f6b63eb7ea10c42d5e431a05f290aebea7d
Andrey <andrey.maht@yandex.ru>
2025-12-25 22:20:00 +0500

    initial commit

---
+++ src/Timus/task_1000/task_2056.java
@@ -0,0 +1,57 @@
+package Timus.task_1000;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class task_2056 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int numberOfExams = in.nextInt();
+
+        int[] array = new int[numberOfExams];
+
+        for (int i = 0; i < numberOfExams; i++) {
+            array[i] = in.nextInt();
+        }
+
+        int sum = 0;
+        boolean hasThree = false;
+        boolean allFives = true;
+
+        for (int i = 0; i < array.length; i++) {
+            int currentGrade = array[i];
+            sum += currentGrade;
+
+            if (currentGrade == 3) {
+                hasThree = true;
+            }
+
+            if (currentGrade != 5) {
+                allFives = false;
+            }
+        }
+
+        double averageScore = (double) sum / numberOfExams;
+
+        String scholarship;
+
+        if (hasThree) {
+            scholarship = "None";
+        } else if (allFives) {
+            scholarship = "Named";
+        } else if (averageScore >= 4.5) {
+            scholarship = "High";
+        } else {
+            scholarship = "Common";
+        }
+
+        System.out.println(scholarship);
+        in.close();
+
+
+    }
+
+}
