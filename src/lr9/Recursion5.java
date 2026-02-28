f853dee882f36832f1fe5886fc851dd492407140
Andrey <andrey.maht@yandex.ru>
2026-02-28 15:48:50 +0500

    initial commit

---
+++ src/lr9/Recursion5.java
@@ -0,0 +1,21 @@
+package lr9;
+
+//Пример 5. Вывести число Фибоначчи, заданное его номером в последовательности.
+
+public class Recursion5 {
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return  0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fact(n - 2) + fact(n - 1);
+        }
+    }
+
+}
