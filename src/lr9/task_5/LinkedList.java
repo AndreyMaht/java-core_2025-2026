bba385f122c2f89e1ab6e7d8e91597fb032b40e0
Andrey <andrey.maht@yandex.ru>
2026-03-14 14:30:21 +0500

    initial commit

--- src/lr9/task_5/LinkedList.java
+++ src/lr9/task_5/LinkedList.java
@@ -13,6 +13,19 @@ public class LinkedList {
         head = newNode;
     }
 
+    public void addTail(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
     public void printList() {
         Node current = head;
         while (current != null) {
--- src/lr9/task_5/unidirectionalList.java
+++ src/lr9/task_5/unidirectionalList.java
@@ -1,14 +1,12 @@
 package lr9.task_5;
 
-//Создание однонаправленного списка с головы
-
 public class unidirectionalList {
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.addHead(10);
-        list.addHead(20);
-        list.addHead(30);
+        list.addTail(10);
+        list.addTail(20);
+        list.addTail(30);
         list.printList();
     }
 
